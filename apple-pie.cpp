eb9d095bccd9fdefe93e20d55bf2ae55c8482188
Nicolas Saputra Gunawan <70703371+Brandy969@users.noreply.github.com>
2021-09-28 10:34:44 +0700

    Create apple-pie.cpp

---
+++ apple-pie.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int apel = A * 3;
+    int hasil = (apel + B) / 2;
+    printf("%d\n", hasil);
+
+    return 0;
+}
