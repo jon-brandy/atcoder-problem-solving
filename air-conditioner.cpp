04001e04cb1d59915cbb30197cc3db6a86cee20d
Nicolas Saputra Gunawan <70703371+Brandy969@users.noreply.github.com>
2021-09-28 10:37:24 +0700

    Create air-conditioner.cpp

---
+++ air-conditioner.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    if(N >= 30)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+    return 0;
+}
