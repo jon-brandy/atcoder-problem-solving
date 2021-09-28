2d71c24ed35397331584512d32f031c8f5e57307
Nicolas Saputra Gunawan <70703371+Brandy969@users.noreply.github.com>
2021-09-28 10:46:03 +0700

    Create five-variables.cpp

---
+++ five-variables.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x1, x2, x3, x4, x5;
+    scanf("%d %d %d %d %d", &x1, &x2, &x3, &x4, &x5);
+    if(x1 == 0)
+    {
+        printf("1\n");
+    }
+    else if(x2 == 0)
+    {
+        printf("2\n");
+    }
+    else if(x3 == 0)
+    {
+        printf("3\n");
+    }
+    else if(x4 == 0)
+    {
+        printf("4\n");
+    }
+    else if(x5 == 0)
+    {
+        printf("5\n");
+    }
+    
+    return 0;
+}
